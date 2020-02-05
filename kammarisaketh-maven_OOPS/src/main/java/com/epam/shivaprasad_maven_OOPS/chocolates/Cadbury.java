c1bb814c13a3d3afb367b3c08d16f343589d9739
saketh01 <37798771+saketh01@users.noreply.github.com>
2020-02-05 11:39:56 +0530

    Add files via upload

---
+++ kammarisaketh-maven_OOPS/src/main/java/com/epam/shivaprasad_maven_OOPS/chocolates/Cadbury.java
@@ -0,0 +1,8 @@
+package com.epam.kammarisaketh_maven_OOPS.chocolates;
+
+public class Cadbury extends Chocolate {
+
+    public Cadbury(String name,int price,int weight){
+        super(name,price,weight);
+    }
+}
---
+++ kammarisaketh-maven_OOPS/src/main/java/com/epam/shivaprasad_maven_OOPS/chocolates/Galaxy.java
@@ -0,0 +1,8 @@
+package com.epam.kammarisaketh_maven_OOPS.chocolates;
+
+public class Galaxy extends Chocolate {
+
+    public Galaxy(String name,int price,int weight){
+        super(name,price,weight);
+    }
+}
